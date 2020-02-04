906f3e5099b1f152711f094c299e7afe44dc6474
ankitjhakal <48992285+ankitjhakal@users.noreply.github.com>
2020-02-04 10:47:03 +0530

    Update logged.php

--- logged.php
+++ logged.php
@@ -2,12 +2,12 @@
 
 
 //session start
-//encrypted username and password
+// if forgot password is pressed redirect to private question page
 if(isset($_POST['forgotpassword']))
 {
   header('location:ques.php');
 }
-
+// else check for successfully login or not
  else{
 include('connection.php');
 mysqli_select_db($conn, "mysql1");
@@ -34,26 +34,5 @@ else {
   echo "connection error";
 }
 }
-/*
-$check = "select * from student";
-$cur_user=null;
-//check if user name is set or not if set then set session user1 variable
-if(isset($_POST['username']))
-{
-  $cur_user=$_POST['username'];
-  //check current user exists or not
-  if(array_key_exists($cur_user,$check))
-  {
-    if($check[$cur_user]==base64_encode($_POST['password']))
-    {
-      echo "successfully login";
-    }
-  }
-}
-else {
-    echo "<h2>Incorrect details</h2>";
-  include('logintest.php');
-}
 
-*/
- ?>
+?>
